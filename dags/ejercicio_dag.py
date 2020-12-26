6b5bcc71c2701b3d1f43da29a28d559874298017
Ignacio Alvarez <ignacionicolasalvarez@gmail.com>
2020-12-26 17:15:07 -0300

    Ejercicio Airflow

--- dags/ejercicio_dag.py
+++ dags/ejercicio_dag.py
@@ -1,31 +1,12 @@
 import datetime as dt
+import os
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.odbc.hooks.odbc import OdbcHook
 from airflow.utils.dates import days_ago
-
 import pandas as pd
-
-
-def get_data():
-
-    try:
-        df = pd.read_csv(
-            "https://gen2cluster.blob.core.windows.net/challenge/csv/nuevas_filas.csv?sv=2019-12-12&ss=b&srt=sco&sp=rx&se=2021-01-31T23:41:30Z&st=2020-12-21T15:41:30Z&spr=https&sig=HGmabI8sYoiQ1%2FXWb7alGqtL0s4ewWXkeAklUhmetqU%3D"
-        )
-    except Exception as e:
-        pass
-
-    print(df.head())
-
-
-def load_data_mssql(**kwargs):
-    odbc_hook = OdbcHook(odbc_conn_id='admin_con', database='Testing_ETL', driver='{ODBC Driver 17 for SQL Server}')
-    query = 'insert into dbo.aux (num) values (1)'
-    odbc_hook.run(query)
-
 
 
 default_args = {
@@ -36,14 +17,13 @@ default_args = {
 }
 
 
-
-def load_data_mssql(**kwargs):
-    odbc_hook = OdbcHook(odbc_conn_id='con_ej_pi_mssql', database='Testing_ETL', driver='{ODBC Driver 17 for SQL Server}')
-    query = 'insert into dbo.aux (num) values (1)'
+def save_data_mssql(query, **kwargs):
+    odbc_hook = OdbcHook(odbc_conn_id='con_ej_pi_mssql',
+                         database='Testing_ETL', driver='{ODBC Driver 17 for SQL Server}')
     odbc_hook.run(query)
 
 
-def extract_data_csv():
+def load_csv(**kwargs):
 
     try:
         df = pd.read_csv(
@@ -51,17 +31,24 @@ def extract_data_csv():
         )
     except Exception as e:
         pass
+    return df
 
-    print(df.head())
+
+def etl_pipe(**kwargs):
+    df = load_csv()
+    df['FECHA_COPIA'] = dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
+    cols = ",".join([str(i) for i in df.columns.tolist()])
+
+    for i, row in df.iterrows():
+        save_data_mssql(
+            f"INSERT INTO [dbo].[Unificado] ({cols}) VALUES {tuple(row)}")
 
 
-with DAG('aaaaaaaaaa_v2',
+with DAG('v5',
          default_args=default_args,
-         schedule_interval='20 * * * *',
+         schedule_interval='0 5 * * 1',
          tags=['PI Consulting']
          ) as dag:
 
-    load_data = PythonOperator(task_id='load_data', python_callable=load_data_mssql)
-
-load_data
-
+    etl_pipe = PythonOperator(task_id='etl_pipe', python_callable=etl_pipe)
